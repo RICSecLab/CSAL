89b7cf875a2d728e1a04a1fbbad9273e8c6d92a9
Akira Moroo <retrage01@gmail.com>
2021-04-27 09:23:23 +0000

    Add workarounds to Marvell ThunderX2 config
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- demos/cs_demo_known_boards.c
+++ demos/cs_demo_known_boards.c
@@ -33,7 +33,7 @@ static int do_registration_thunderx2(struct cs_devices_t *devices)
     // uintptr_t pmu_base = 0x410430000;
     uintptr_t etm_base = 0x410440000;
 
-    cs_device_t rep, etr, etf, funnel;
+    cs_device_t rep, tpiu, etr, etf, funnel;
 
     if (registration_verbose)
         printf("CSDEMO: Registering ThunderX2 CoreSight devices...\n");
@@ -56,15 +56,31 @@ static int do_registration_thunderx2(struct cs_devices_t *devices)
         printf("CSDEMO: Registering trace-bus connections...\n");
 
     funnel = cs_device_get(0x410001000);
+    /* FIXME: funnel has 3 in ports. Hardcode to connect CPU #0 to #2 */
+    cs_atb_register(cs_cpu_get_device(0, CS_DEVCLASS_SOURCE), 0,
+		    funnel, 0);
+    cs_atb_register(cs_cpu_get_device(1, CS_DEVCLASS_SOURCE), 0,
+		    funnel, 1);
+    cs_atb_register(cs_cpu_get_device(2, CS_DEVCLASS_SOURCE), 0,
+		    funnel, 2);
+
     etf = cs_device_get(0x410002000);
-    rep = cs_device_get(0x410003000);
+    cs_atb_register(funnel, 0, etf, 0);
+
+    rep = cs_atb_add_replicator(2);
+    cs_atb_register(etf, 0, rep, 0);
+
     etr = cs_device_get(0x410004000);
+    tpiu = cs_device_get(0x410005000);
 
-    cs_atb_register(funnel, 0, etf, 0); // from funnel to etf
-    cs_atb_register(etf, 0, rep, 0); // from etf to replicator
-    cs_atb_register(rep, 0, etr, 0); // from replicator to etr (on memory buffer) 
+    cs_atb_register(rep, 0, etr, 0);
+    cs_atb_register(rep, 1, tpiu, 0);
 
-    devices->etb = etr;
+    devices->etb = etf;
+
+    for (int i = 0; i < num_cs_cpu; i++ ) {
+        devices->cpu_id[i] = cpu_id[i];
+    }
 
     return 0;
 }
